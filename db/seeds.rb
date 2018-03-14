1f319c99a9f408ba719926732d72fc0878cfb931
amurillojr92 <amurillojr92@gmail.com>
2018-03-14 07:08:50 -0400

    added admin features

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,3 +68,81 @@ Style: 886697-473
 <p>},
   image_url: 'nikebluehoodie.jpg',
   price: 100.00)
+# . . .
+
+Product.create!(title: 'FC BARCELONA',
+  description:
+    %{<p>COOL-WEATHER COVERAGE
+
+
+The FC Barcelona Men's Full-Zip Hoodie features team details on soft French terry fabric for a warm, comfortable feel.
+
+
+Benefits
+
+French terry fabric feels soft and warm
+Adjustable hood offers personalized coverage
+Rib cuffs and hem help keep the hoodie in place
+Split kangaroo pocket
+
+Product Details
+
+Fabric: Body/hood lining: 100% cotton. Rib: 98% cotton/2% spandex.
+Machine wash
+Imported
+Shown: Obsidian Heather/Hyper Crimson
+Style: 886697-473
+
+<p>},
+  image_url: 'nikebluehoodie.jpg',
+  price: 100.00)
+# . . .
+
+Product.create!(title: 'NIKE AIR MAX 270',
+  description:
+    %{<p>The Nike Air Max 270 Men's Shoe is inspired by two icons of big Air: the Air Max 180 and Air Max 93. It features Nike’s biggest heel Air unit yet for a super-soft ride that feels as impossible as it looks.
+
+
+FLEXIBLE AND BREATHABLE
+
+The shoe's bootie construction feels snug and flexes with your foot. It's made with solid and mesh areas, giving you structure and ventilation where you need them most.
+
+
+SUPER-SOFT CUSHIONING
+
+A dual-density foam sole and large Max Air heel unit deliver plush cushioning for casual, everyday comfort.
+
+
+
+<p>},
+  image_url: 'nikepurple2.png',
+  price: 150.00)
+# . . .
+
+Product.create!(title: 'NIKE THERMA ELITE',
+  description:
+    %{<p>The Nike Therma Elite Men's Basketball Pants help keep you warm and comfortable on the outdoor court with sweat-wicking thermal fabric.
+
+
+Benefits
+
+Nike Therma fabric helps keep you warm
+Dri-FIT Technology helps keep you dry and comfortable
+Two-way stretch allows natural movement
+Elastic waistband with drawcord for a snug, comfortable fit
+Drawcord at hem provides a stay-put fit
+Side seam pockets
+
+Product Details
+
+Fabric: Dri-FIT 100% polyester
+Machine wash
+Imported
+Shown: Team Black/Team White/Team White
+Style: 835384-012
+
+
+<p>},
+  image_url: 'niketherma2.jpg',
+  price: 70.00)
+
